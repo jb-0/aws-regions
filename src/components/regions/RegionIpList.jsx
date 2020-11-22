2d9779a0914e26001f13f8ed68e5afdfe6fa2b50
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-22 15:47:24 +0000

    Added and styled a table that presents the ip addresses for a given region

--- src/components/regions/RegionIpList.jsx
+++ src/components/regions/RegionIpList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import getAwsIpRanges from '../../data/getAwsIpRanges';
+import './RegionIpList.css';
 
 function RegionIpList(props) {
   const [regionIpData, setRegionIpData] = useState();
@@ -8,11 +9,40 @@ function RegionIpList(props) {
     async function setData() {
       setRegionIpData(await getAwsIpRanges('eu-west-1'));
     }
-    
-    setData()
+
+    setData();
   }, []);
 
-  return <div>{regionIpData && <div>{regionIpData.map((ip) => ip.region)}</div>}</div>;
-};
+  return (
+    <div className="region-ip-list-container">
+      <table className="region-ip-list-table">
+        <thead>
+          <tr>
+            <th>ip_prefix</th>
+            <th>region</th>
+            <th>service</th>
+            <th>network_border_group</th>
+          </tr>
+        </thead>
+        <tbody>
+          {regionIpData && (
+            <React.Fragment>
+              {regionIpData.map((ip) => {
+                return (
+                  <tr>
+                    <td>{ip.ip_prefix}</td>
+                    <td>{ip.region}</td>
+                    <td>{ip.service}</td>
+                    <td>{ip.network_border_group}</td>
+                  </tr>
+                );
+              })}
+            </React.Fragment>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 export default RegionIpList;
