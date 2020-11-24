b7e642b168470ba556713ccd92d9f1612aa534d1
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-24 06:21:50 +0000

    Updated regions array to object to include country codes and region names

--- src/data/awsRegions.js
+++ src/data/awsRegions.js
@@ -1,27 +1,31 @@
-const regions = [
-  'us-east-1',
-  'us-east-2',
-  'us-west-1',
-  'us-west-2',
-  'af-south-1',
-  'ap-east-1',
-  'ap-south-1',
-  'ap-northeast-3',
-  'ap-northeast-2',
-  'ap-southeast-1',
-  'ap-southeast-2',
-  'ap-northeast-1',
-  'ca-central-1',
-  'eu-central-1',
-  'eu-west-1',
-  'eu-west-2',
-  'eu-south-1',
-  'eu-west-3',
-  'eu-north-1',
-  'me-south-1',
-  'sa-east-1',
-  'us-gov-east-1',
-  'us-gov-west-1',
-];
+const regions = {
+  'us': [
+    {'us-east-1': 'N Virginia'},
+    {'us-east-2': 'Ohio'},
+    {'us-west-1': 'Northern California'},
+    {'us-west-2': 'Oregon'},
+    {'us-gov-east-1': 'AWS GovCloud (US-East)'},
+    {'us-gov-west-1': 'AWS GovCloud (US-East)'}],
+  'za': 
+    [{'af-south-1': 'Cape Town'}],
+  'hk': 
+    [{'ap-east-1': 'Hong Kong'}],
+  'in': 
+    [{'ap-south-1': 'Mumbai'}],
+  'jp':
+    [{'ap-northeast-3': 'Osaka-Local', 'ap-northeast-1': 'Tokyo' }],
+  'kr': [{'ap-northeast-2': 'Seoul'}],
+  'sg': [{'ap-southeast-1': 'Singapore'}],
+  'au': [{'ap-southeast-2': 'Sydney'}],
+  'ca': [{'ca-central-1': 'Canada'}],
+  'de': [{'eu-central-1': 'Frankfurt'}],
+  'ie': [{'eu-west-1': 'Ireland'}],
+  'gb': [{'eu-west-2': 'London'}],
+  'it': [{'eu-south-1': 'Milan'}],
+  'fr': [{'eu-west-3': 'Paris'}],
+  'se': [{'eu-north-1': 'Stockholm'}],
+  'bh': [{'me-south-1': 'Bahrain'}],
+  'br': [{'sa-east-1': 'Sao Paulo'}],
+};
 
 export default regions;
