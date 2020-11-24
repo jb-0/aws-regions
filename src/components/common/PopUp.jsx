ce28a7b248c08248aa4df59762ed6af673406e85
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-24 19:42:55 +0000

    Styled popup and mapped regions for a given country so they are displayed in popup

--- src/components/common/PopUp.jsx
+++ src/components/common/PopUp.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './PopUp.css';
+import regions from '../../data/awsRegions';
 
 function PopUp(props) {
-  
   return (
     <div
-      style={{ top: props.mousePosition.y, left: props.mousePosition.x }}
+      style={{ top: props.mousePosition.y + 20, left: props.mousePosition.x }}
       className="popup-container"
     >
-      Popup Container
+      <h3>{props.clickedCountry.name}</h3>
+      {regions[props.clickedCountry.code].map((region) => {
+        return <p>{`${region.niceName} - ${region.name}`}</p>
+      })}
     </div>
   );
 }
