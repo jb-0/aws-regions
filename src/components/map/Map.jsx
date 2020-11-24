f82d961e9aa278da73bd15dd5e7217cf3dc6ee97
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-24 19:29:07 +0000

    Updated layer prop function to include country id

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { VectorMap } from '@south-paw/react-vector-maps';
-import PopUp from '../common/PopUp'
+import PopUp from '../common/PopUp';
 import world from '../../data/worldMap.json';
 import './Map.css';
 
@@ -8,23 +8,35 @@ function Map(props) {
   const [mousePosition, setMousePosition] = useState();
   const [showPopup, setShowPopup] = useState(false);
 
-  // For each layer of the map add an onclick function that sets the clicked country state to the 
+  // For each layer of the map add an onclick function that sets the clicked country state to the
   // country name
   const layerProps = {
-    onClick: ({ target }) => props.setClickedCountry(target.attributes.name.value)
+    onClick: ({ target }) =>
+      props.setClickedCountry({
+        name: target.attributes.name.value,
+        code: target.attributes.id.value,
+      }),
   };
 
   // If the user clicks anywhere in the map div store the point in time mouse coordinates and show
   // the popup component
   function onClick(event) {
-    setMousePosition({ x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY });
+    setMousePosition({
+      x: event.nativeEvent.offsetX,
+      y: event.nativeEvent.offsetY,
+    });
     setShowPopup(true);
   }
 
   return (
     <div className="map" onClick={onClick}>
       <VectorMap {...world} layerProps={layerProps} />
-      {showPopup && <PopUp mousePosition={mousePosition} clickedCountry={props.clickedCountry}/>}
+      {showPopup && (
+        <PopUp
+          mousePosition={mousePosition}
+          clickedCountry={props.clickedCountry}
+        />
+      )}
     </div>
   );
 }
