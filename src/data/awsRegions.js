9bd51ae968cacddfe89d38cb9ee63c38ced2137a
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-24 19:42:24 +0000

    Updated region data to include name and nicename keys rathern than just having the region as a key and nicename as it's value

--- src/data/awsRegions.js
+++ src/data/awsRegions.js
@@ -1,31 +1,31 @@
 const regions = {
   'us': [
-    {'us-east-1': 'N Virginia'},
-    {'us-east-2': 'Ohio'},
-    {'us-west-1': 'Northern California'},
-    {'us-west-2': 'Oregon'},
-    {'us-gov-east-1': 'AWS GovCloud (US-East)'},
-    {'us-gov-west-1': 'AWS GovCloud (US-East)'}],
+    {name: 'us-east-1', niceName: 'N Virginia'},
+    {name: 'us-east-2', niceName: 'Ohio'},
+    {name: 'us-west-1', niceName: 'Northern California'},
+    {name: 'us-west-2', niceName: 'Oregon'},
+    {name: 'us-gov-east-1', niceName: 'AWS GovCloud (US-East)'},
+    {name: 'us-gov-west-1', niceName: 'AWS GovCloud (US-East)'}],
   'za': 
-    [{'af-south-1': 'Cape Town'}],
+    [{name: 'af-south-1', niceName: 'Cape Town'}],
   'hk': 
-    [{'ap-east-1': 'Hong Kong'}],
+    [{name: 'ap-east-1', niceName: 'Hong Kong'}],
   'in': 
-    [{'ap-south-1': 'Mumbai'}],
+    [{name: 'ap-south-1', niceName: 'Mumbai'}],
   'jp':
-    [{'ap-northeast-3': 'Osaka-Local', 'ap-northeast-1': 'Tokyo' }],
-  'kr': [{'ap-northeast-2': 'Seoul'}],
-  'sg': [{'ap-southeast-1': 'Singapore'}],
-  'au': [{'ap-southeast-2': 'Sydney'}],
-  'ca': [{'ca-central-1': 'Canada'}],
-  'de': [{'eu-central-1': 'Frankfurt'}],
-  'ie': [{'eu-west-1': 'Ireland'}],
-  'gb': [{'eu-west-2': 'London'}],
-  'it': [{'eu-south-1': 'Milan'}],
-  'fr': [{'eu-west-3': 'Paris'}],
-  'se': [{'eu-north-1': 'Stockholm'}],
-  'bh': [{'me-south-1': 'Bahrain'}],
-  'br': [{'sa-east-1': 'Sao Paulo'}],
+    [{name: 'ap-northeast-3', niceName: 'Osaka-Local', 'ap-northeast-1': 'Tokyo' }],
+  'kr': [{name: 'ap-northeast-2', niceName: 'Seoul'}],
+  'sg': [{name: 'ap-southeast-1', niceName: 'Singapore'}],
+  'au': [{name: 'ap-southeast-2', niceName: 'Sydney'}],
+  'ca': [{name: 'ca-central-1', niceName: 'Canada'}],
+  'de': [{name: 'eu-central-1', niceName: 'Frankfurt'}],
+  'ie': [{name: 'eu-west-1', niceName: 'Ireland'}],
+  'gb': [{name: 'eu-west-2', niceName: 'London'}],
+  'it': [{name: 'eu-south-1', niceName: 'Milan'}],
+  'fr': [{name: 'eu-west-3', niceName: 'Paris'}],
+  'se': [{name: 'eu-north-1', niceName: 'Stockholm'}],
+  'bh': [{name: 'me-south-1', niceName: 'Bahrain'}],
+  'br': [{name: 'sa-east-1', niceName: 'Sao Paulo'}],
 };
 
 export default regions;
