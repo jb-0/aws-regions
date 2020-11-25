58f5295d59cd26c5ff867ca5ec64210bd92ab386
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-25 06:09:02 +0000

    Added react router and setup routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 // import { useEffect, useState } from 'react';
 import './App.css';
-import Navbar from './components/common/Navbar'
-import MapPage from './components/pages/MapPage'
-import RegionIpList from './components/regions/RegionIpList';
-
+import Navbar from './components/common/Navbar';
+import MapPage from './components/pages/MapPage';
+import RegionList from './components/regions/RegionList';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <MapPage />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={MapPage} />
+          <Route path="/map" exact component={MapPage} />
+          <Route path="/list" exact component={RegionList} />
+        </Switch>
+      </Router>
     </div>
   );
 }
