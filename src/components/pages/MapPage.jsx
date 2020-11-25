a123ac599dc64f4198b506e1936d8422b2480539
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-25 06:24:55 +0000

    Added an intro paragraph to explain how to use the map

--- src/components/pages/MapPage.jsx
+++ src/components/pages/MapPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Map from '../map/Map';
-import RegionIPList from '../regions/RegionIpList'
+import RegionIPList from '../regions/RegionIpList';
 import './MapPage.css';
 
 function MapPage() {
@@ -9,17 +9,18 @@ function MapPage() {
 
   return (
     <div className="map-page-container">
+      <p className="map-intro-para">
+        Countries with at least one AWS region are highlighted in blue, click a
+        highlighted country to see list of AWS regions. Clicking a region will
+        return a list of IPs.
+      </p>
       <Map
         clickedCountry={clickedCountry}
         setClickedCountry={setClickedCountry}
         clickedRegion={clickedRegion}
         setClickedRegion={setClickedRegion}
       />
-      {clickedRegion && (
-        <RegionIPList
-          region={clickedRegion}
-        />
-      )}
+      {clickedRegion && <RegionIPList region={clickedRegion} />}
     </div>
   );
 }
