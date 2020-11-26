da1185993685f15eb4d1567572e3ae08e977b4cb
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-26 19:43:01 +0000

    Migrated popup from p tags to buttons

--- src/components/common/PopUp.jsx
+++ src/components/common/PopUp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './PopUp.css';
-import {regions} from '../../data/awsRegions';
+import { regions } from '../../data/awsRegions';
 import { v4 as uuidv4 } from 'uuid';
 
 function PopUp(props) {
@@ -16,12 +16,14 @@ function PopUp(props) {
       <h3 className="popup">{props.clickedCountry.name}</h3>
       {regions[props.clickedCountry.code].map((region) => {
         return (
-          <p 
+          <button
             key={uuidv4()}
             id={region.name}
             onClick={handleClick}
             className="popup"
-          >{`${region.niceName} - ${region.name}`}</p>
+          >
+            {region.name}
+          </button>
         );
       })}
     </div>
