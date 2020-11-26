2cf343b422d7e9b8f7ceac524503a5423001a8e2
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-26 18:11:11 +0000

    Implemented css grid for countries and enhanced the information displayed per country

--- src/components/regions/RegionList.jsx
+++ src/components/regions/RegionList.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import {regions, countries} from '../../data/awsRegions';
-import './RegionList.css'
+import { regions, countries } from '../../data/awsRegions';
+import './RegionList.css';
 
 function RegionList() {
-  Object.entries(regions).forEach(([key, val]) => {});
-
   return (
-    <div className="region-list">
+    <div className="region-list-grid">
       {Object.entries(regions).map(([key, value]) => {
-        return value.map((region) => {
-          return <h2>{region.niceName}</h2>;
-        });
+        return (
+          <div className={`country-grid-item ${key}`}>
+            <h2>{countries[key]}</h2>
+            {value.map((region) => {
+              return <p>{`${region.niceName} - ${region.name} `}</p>;
+            })}
+          </div>
+        );
       })}
     </div>
   );
