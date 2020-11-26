ecd6433c18c16d150bda5d75e31d5ba948059aaf
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-26 07:36:53 +0000

    Iterated through regions object to print region nice names on region list

--- src/components/regions/RegionList.jsx
+++ src/components/regions/RegionList.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import regions from '../../data/awsRegions';
 
 function RegionList() {
-  return (<p>region list</p>)
-};
+  Object.entries(regions).forEach(([key, val]) => {});
+
+  return (
+    <div className="region-list">
+      {Object.entries(regions).map(([key, value]) => {
+        return value.map((region) => {
+          return <h1>{region.niceName}</h1>;
+        });
+      })}
+    </div>
+  );
+}
 
 export default RegionList;
