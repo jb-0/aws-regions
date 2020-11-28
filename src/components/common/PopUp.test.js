34dac4d2cba437961873e64fd939d9f44aedb169
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-28 15:10:02 +0000

    Added a test to assert that clicking a region invokes setClickedRegion

--- src/components/common/PopUp.test.js
+++ src/components/common/PopUp.test.js
@@ -1,7 +1,8 @@
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import PopUp from './PopUp';
-import { getByText, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 
 describe('Pop up component tests', () => {
@@ -45,4 +46,26 @@ describe('Pop up component tests', () => {
     expect(screen.getByText('ap-northeast-1')).toBeInTheDocument()
     expect(screen.getByText('ap-northeast-3')).toBeInTheDocument()
   });
+
+  it('clicking a region invokes setClickedRegion and passes the region as an argument', () => {
+    const setClickedRegion = jest.fn();
+
+    act(() => {
+      render(
+        <PopUp
+          mousePosition={{x: 10, y: 10}}
+          clickedCountry={{code: 'jp', name:'Japan'}}
+          setClickedRegion={setClickedRegion}
+        />,
+        container
+      );
+    });
+
+    // Click one of the regions that is displayed for japan
+    userEvent.click(screen.getByText('ap-northeast-1'))
+
+    // Expect the function to have been called once and the region to have been passed as an arg
+    expect(setClickedRegion.mock.calls.length).toBe(1);
+    expect(setClickedRegion.mock.calls[0][0]).toBe('ap-northeast-1');
+  })
 });
