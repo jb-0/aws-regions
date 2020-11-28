0c9184b7936d2de07217178833f7f9a166ebcd55
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-28 14:26:38 +0000

    Added comments to support code

--- src/components/common/PopUp.jsx
+++ src/components/common/PopUp.jsx
@@ -4,11 +4,14 @@ import { regions } from '../../data/awsRegions';
 import { v4 as uuidv4 } from 'uuid';
 
 function PopUp(props) {
+  // Set the region state to the region that was clicked using it's id
   function handleClick(event) {
     props.setClickedRegion(event.target.id);
   }
 
   return (
+    // Create the popup as a div which is located just below (+20) where the user clicked on the
+    // country, the popup will list all regions for a given country
     <div
       style={{ top: props.mousePosition.y + 20, left: props.mousePosition.x }}
       className="card-style popup-container popup"
