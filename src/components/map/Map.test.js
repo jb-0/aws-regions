01b53c933e99f95f5b753c51f7dbc3168772ef11
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-29 18:09:07 +0000

    Added test to ensure that when a country is clicked a popup is rendered

--- src/components/map/Map.test.js
+++ src/components/map/Map.test.js
@@ -1,6 +1,6 @@
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Map from './Map';
 
@@ -37,5 +37,36 @@ describe('Map component test', () => {
     });
     
     expect(screen.queryAllByLabelText('World Low Res').length).toBe(1)
+  });
+
+  it('clicking a country opens a PopUp container with the countries regions', async () => {
+    const setClickedRegion = jest.fn();
+    const setClickedCountry = jest.fn();
+
+    // Render component with Canada as the clicked country, as there has been no click event the
+    // popup will not be displayed
+    act(() => {
+      render(
+        <Map
+          clickedCountry={{code: 'ca', name:'Canada'}}
+          setClickedRegion={setClickedRegion}
+          setClickedCountry={setClickedCountry}
+        />,
+        container
+      );
+    });
+
+    const event = new MouseEvent('click', { bubbles: true });
+
+    // Assign the event target coordinates, these are passed through to position the popup component
+    Object.defineProperty(event, 'offsetX', {get: () => 1})
+    Object.defineProperty(event, 'offsetY', {get: () => 1})
+  
+    // Click on Canada to invoke the popup (could click any valid country as the clickedCountry has
+    // been passed as a prop)
+    fireEvent(screen.getByLabelText('Canada'), event)
+    
+    // Expect the popup to be rendered which will contain the Canada region
+    expect(screen.getByText('ca-central-1')).toBeInTheDocument()
   });
 });
