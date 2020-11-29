6ff0a479a60a2e68af140b4f9372013a1e47cff8
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-29 13:14:25 +0000

    Added comments to test to support code

--- src/data/awsRegions.test.js
+++ src/data/awsRegions.test.js
@@ -4,10 +4,13 @@ describe('AWS Region static data validation', () => {
   it('each country code in the countries object exists in the regions object', () => {
     const countryCodesNotPresent = [];
 
+    // Iterate countries object and for each country code (key) check if it does not exist in
+    // regions, if it does not exist then add it to an array of country codes not present
     Object.entries(countries).map(([key, value]) => {
       if (!regions[key]) countryCodesNotPresent.push(key)
     });
 
+    // We expect all country codes to be present, therefore expect an empty array to be returned
     expect(countryCodesNotPresent).toEqual([])
   })
 });
