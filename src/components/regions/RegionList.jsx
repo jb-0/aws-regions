bcd95f6aca4add1d82fc030f319de483c5bb936c
Jamie Barrett <timothybarrett@hotmail.co.uk>
2020-11-30 06:34:09 +0000

    Added comments to support code

--- src/components/regions/RegionList.jsx
+++ src/components/regions/RegionList.jsx
@@ -4,6 +4,8 @@ import './RegionList.css';
 import { v4 as uuidv4 } from 'uuid';
 
 function RegionList(props) {
+  // When a region is clicked, set the clicked country and country code, the country code is taken
+  // from the elements classList
   function onClick(event) {
     const countryCode = event.currentTarget.parentNode.classList[2];
     const countryName = countries[countryCode];
@@ -17,10 +19,14 @@ function RegionList(props) {
 
   return (
     <div className="region-list-grid">
+
+    {/* Iterate each item in the regions object */}
       {Object.entries(regions).map(([key, value]) => {
         return (
           <div className={`card-style country-grid-item ${key}`}>
             <h2>{countries[key]}</h2>
+
+            {/* For each value (country) in the regions object, iterate through it's regions */}
             {value.map((region) => {
               return (
                 <button
